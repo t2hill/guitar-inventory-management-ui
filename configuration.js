77d77cea0084dc11c30f31a30e5b1b9d90e462e4
toohill.ted@gmail.com <toohill.ted@gmail.com>
2024-04-14 12:05:26 -0400

    hw8 changes

---
+++ configuration.js
@@ -0,0 +1,84 @@
+const mode = 0;
+
+const host_local = "http://localhost:8080";
+// const host_remote = "https://lab9-g4r5.onrender.com";
+
+function getHost() {
+    return (mode == 0) ? host_local : host_remote;
+}
+
+function isLoggedIn() {
+    if(localStorage.getItem("token")) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function getTheToken() {
+    return localStorage.getItem("token");
+}
+
+function saveTheToken(token) {
+    localStorage.setItem("token", token);
+    updateTheNavigationBar();
+}
+
+function removeTheToken() {
+    localStorage.removeItem("token");
+    updateTheNavigationBar();
+}
+
+let configuration = {
+    isLoggedIn: () => isLoggedIn(),
+    host: () => getHost(),
+    token: () => getTheToken()
+};
+
+updateTheNavigationBar();
+
+async function updateTheNavigationBar() {
+    const navigation = document.getElementsByClassName("navbar")[0];
+    let loginTag = navigation.children[navigation.children.length - 2];
+    if(configuration.isLoggedIn()) {
+        loginTag.innerHTML = 
+        `<li><a href="login.html" onclick="logout()">Logout</a></li>`;
+    } else {
+        loginTag.innerHTML = `<li><a href="login.html">Login</a></li>`;
+    }
+}
+
+async function login() {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let customer = {username: username, password: password}
+    let request = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(customer)
+    };
+    try {
+        let response = await fetch(getHost() + "/signin", request);
+        if (response.status = 200) {
+            alert("The login was successful!");
+            const token = await response.text();
+            saveTheToken(token);
+            location.href = "index.html";
+        } else {
+            console.log(`response status:${response.status}`);
+            removeTheToken();
+            alert("Something went wrong!");
+        }
+    }
+    catch(error) {
+        console.log(error);
+        removeTheToken();
+        alert("Something went wrong!");
+    }
+}
+
+async function logout() {
+    removeTheToken();
+}
