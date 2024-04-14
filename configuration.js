250315e05354e13c98bfa7479aa416d7e435857d
toohill.ted@gmail.com <toohill.ted@gmail.com>
2024-04-14 12:10:54 -0400

    hw8 changes

--- configuration.js
+++ configuration.js
@@ -1,7 +1,7 @@
 const mode = 0;
 
 const host_local = "http://localhost:8080";
-// const host_remote = "https://lab9-g4r5.onrender.com";
+const host_remote = "https://gim-hw8.onrender.com";
 
 function getHost() {
     return (mode == 0) ? host_local : host_remote;
